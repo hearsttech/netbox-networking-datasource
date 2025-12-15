b2c198d142863dcda82e30364748673704e51d82
Jay <jay.yaldor@hearst.com>
2025-12-15 08:39:33 -0500

    Removed Folder

---
+++ scripts-mater-switch-position-fix.py
@@ -0,0 +1,25 @@
+from extras.scripts import Script
+from dcim.models import Device, VirtualChassis
+
+
+class StackMasterPositionFix(Script):
+    class Meta:
+        name = "Stack Master Position Fix"
+        description = "Assigns correct position to Master switch based on naming convention."
+    def run(self, data, commit):
+        data_obj = data
+        # Confirms that Objects Created by Event are Present
+        if not data_obj or not isinstance(data_obj, dict):
+            self.log_failure("No valid object found in event data.")
+            return
+        switch_name = data_obj.get('name')
+        switch = Device.objects.get(name=switch_name)
+        virtual_chassis_name = switch_name.rsplit("-", 1)[0] + "-Stack"
+        if VirtualChassis.objects.get(name=virtual_chassis_name):
+            virtual_chassis = VirtualChassis.objects.get(name=virtual_chassis_name)
+            if switch == virtual_chassis.master:
+                switch.vc_position = switch_name.rsplit("-", 1)[1]
+                switch.save()
+                self.log_success(f"Successfully set {switch_name} position to Master {switch.vc_position} in {virtual_chassis_name}")
+            else:
+                self.log_info(f"{switch_name} is not the master switch in {virtual_chassis_name}, no changes made.")
