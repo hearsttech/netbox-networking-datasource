235a666bc6b52dee89f1ce5581b56ed61c1653ee
Jay <jay.yaldor@hearst.com>
2026-01-21 14:18:37 -0500

    error fixing

--- scripts-mater-switch-position-fix.py
+++ scripts-mater-switch-position-fix.py
@@ -13,13 +13,20 @@ class StackMasterPositionFix(Script):
             self.log_failure("No valid object found in event data.")
             return
         switch_name = data_obj.get('name')
+        self.log_info(f"Processing switch: {switch_name}")
         switch = Device.objects.get(name=switch_name)
         virtual_chassis_name = switch_name.rsplit("-", 1)[0] + "-Stack"
-        if VirtualChassis.objects.get(name=virtual_chassis_name):
-            virtual_chassis = VirtualChassis.objects.get(name=virtual_chassis_name)
-            if switch == virtual_chassis.master:
-                switch.vc_position = switch_name.rsplit("-", 1)[1]
-                switch.save()
-                self.log_success(f"Successfully set {switch_name} position to Master {switch.vc_position} in {virtual_chassis_name}")
-            else:
-                self.log_info(f"{switch_name} is not the master switch in {virtual_chassis_name}, no changes made.")
+        self.log_info(f"Derived Virtual Chassis name: {virtual_chassis_name}")
+        try:
+            VirtualChassis.objects.get(name=virtual_chassis_name)
+        except VirtualChassis.DoesNotExist:
+            self.log_failure(f"Virtual Chassis {virtual_chassis_name} does not exist. No changes made.")
+        else:
+            if VirtualChassis.objects.get(name=virtual_chassis_name):
+                virtual_chassis = VirtualChassis.objects.get(name=virtual_chassis_name)
+                if switch == virtual_chassis.master:
+                    switch.vc_position = switch_name.rsplit("-", 1)[1]
+                    switch.save()
+                    self.log_success(f"Successfully set {switch_name} position to Master {switch.vc_position} in {virtual_chassis_name}")
+                else:
+                    self.log_info(f"{switch_name} is not the master switch in {virtual_chassis_name}, no changes made.")
