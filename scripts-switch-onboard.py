36e43fcb6100fd5130e4dbd95904862a4c80639e
Jay <jay.yaldor@hearst.com>
2026-02-23 15:39:30 -0500

    choice testing

--- scripts-switch-onboard.py
+++ scripts-switch-onboard.py
@@ -22,4 +22,9 @@ class SwitchOnboard(Script):
         description="Enter the IP address of the switch (e.g., 192.168.1.1).",
         required=True,
     )
-    var = ChoiceVar(choices=("test", "TESTING"))
+    var = ChoiceVar(
+        choices=(("#ff0000", "Red"), ("#00ff00", "Green"), ("#0000ff", "Blue")),
+        label="Color",
+        description="Select a color for the switch.",
+        required=False,
+    )
